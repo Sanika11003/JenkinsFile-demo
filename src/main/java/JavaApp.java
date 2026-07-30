160479ad97a4e15721170f4d4b44dcb417764d98
Sanika <sanikabhosale73@gmail.com>
2026-07-30 10:15:02 +0000

    Add maven java project

---
+++ src/main/java/JavaApp.java
@@ -0,0 +1,10 @@
+public class JavaApp {
+ 
+   public static void main(String[] args) {
+ 
+      System.out.println("Hello from Maven!");
+      System.out.println("Welcome to Jenkins CI/CD");
+ 
+   }
+}
+ 
